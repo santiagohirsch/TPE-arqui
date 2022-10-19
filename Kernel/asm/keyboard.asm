422e84fc0f5c5c9bfab1236baec10ac3fc12afb4
maurov <mvella@itba.edu.ar>
2022-10-18 21:52:20 -0300

    CleanUp

--- Kernel/asm/keyboard.asm
+++ Kernel/asm/keyboard.asm
@@ -1,26 +1,34 @@
 GLOBAL getKey
 GLOBAL tKey
+
 section .text
+
 getKey:
-push rbp
-mov rbp, rsp
-mov rax, 0
+    push rbp
+    mov rbp, rsp
+
+    mov rax, 0
+
 loop: 
-in al, 0x64
-mov cl, al
-and al, 0x01
-cmp al, 0
-je loop
-in al ,0x60
-mov rsp, rbp
-pop rbp
-ret
+    in al, 0x64
+    mov cl, al
+    and al, 0x01
+    cmp al, 0
+
+    je loop
+    in al ,0x60
+
+    mov rsp, rbp
+    pop rbp
+    ret
 
 tKey:
-push rbp
-mov rbp, rsp
-mov rax, 0
-in al, 0x60
-mov rsp, rbp
-pop rbp
-ret
+    push rbp
+    mov rbp, rsp
+
+    mov rax, 0
+    in al, 0x60
+    
+    mov rsp, rbp
+    pop rbp
+    ret
