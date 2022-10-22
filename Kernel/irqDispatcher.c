0316bb20d8344002d7073913b2c7f1479d793f9b
shirsch <shirsch@itba.edu.ar>
2022-10-22 19:51:24 -0300

    little changes

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -7,8 +7,6 @@ static void int_20();
 
 static void int_21();
 
-static void int_30();
-
 void irqDispatcher(uint64_t irq) {
 	switch (irq) {
 		case 0:
@@ -17,8 +15,6 @@ void irqDispatcher(uint64_t irq) {
 		case 1:
 			int_21();
 			break;
-		case 10:
-			int_30();
 	}
 	return;
 }
@@ -31,8 +27,4 @@ void int_20() {
 
 void int_21(){
 	keyboard_handler();
-}
-
-void int_30(){
-	
 }
--- Userland/SampleCodeModule/asm/syscalls.asm
+++ Userland/SampleCodeModule/asm/syscalls.asm
@@ -4,7 +4,7 @@ GLOBAL sys_read
 section .text
 sys_read:
     mov rax, 0x00
-    syscall
+    int 80h
     ret
 sys_write:
     mov rax, 0x01
