56a636a294c79c7ab1a3e1afa3a30c94569731c7
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-22 16:15:11 -0300

    changes on idtLoader

--- Kernel/idtLoader.c
+++ Kernel/idtLoader.c
@@ -14,7 +14,7 @@ typedef struct {
   uint32_t offset_h, other_cero;
 } DESCR_INT;
 
-#pragma pack(pop)		/* Reestablece la alinceación actual */
+#pragma pack(pop)		/* Reestablece la alineación actual */
 
 
 
@@ -28,7 +28,6 @@ void load_idt() {
 
     setup_IDT_entry (0x20, (uint64_t)&_irq00Handler);
     setup_IDT_entry (0x21, (uint64_t)&_irq01Handler);
-    setup_IDT_entry (0x30, (uint64_t)&_irq10Handler);
     setup_IDT_entry (0x80, (uint64_t)&_irq80Handler);
     setup_IDT_entry (0x00, (uint64_t)&_exception0Handler);
     
