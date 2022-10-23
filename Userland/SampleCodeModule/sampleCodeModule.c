4029c18d8fcb3c99246c4374dad2e4b94316aa3c
sofialtman <70671805+sofialtman@users.noreply.github.com>
2022-10-23 20:15:55 -0300

    help display implementation

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,5 +1,6 @@
 #include <libVid.h>
 #include <stdint.h>
+#include <string.h>
 
 #define MAX_PARAMETERS 10
 #define LENGTH_PARAMETERS 20
@@ -8,7 +9,23 @@
 
 /* COMMANDS */
 static void help(){
+	/*if(parameters[0][0]!=0){
+		print("Try help without parameters\n");
+		return;
+	}*/
 	
+	const char* helpstring = 
+	"HELP                 Provides help information for commands.\n"
+	"SCREENSHOT           Register's screenshot.\n"
+	"INVALIDOPCODE        Command to verify the operation of the exception routine \"Invalid Opcode\"\n"
+	"DIVIDEBYZERO         Command to verify the operation of the exception routine \"Divide by zero\"\n"
+	"INFOREG              Prints on screen the value of all registers.\n"
+	"PRINTMEM             Receives as argument a pointer and performs a memory dump of 32 bytes from the\n"
+	".                    Address received as an argument.\n"
+	"TIME                 Command to display the system day and time.\n"
+	"CHANGEFONTSIZE       Changes font size.\n"
+	"TRON                 Get ready to play Tron!.\n";
+	print(helpstring);
 }
 
 static void screenshot(){
@@ -46,24 +63,6 @@ static void tron(){
 }
 
 
-//iria aca ?
-static int strcmp(char *str1, char *str2){
-	while( ( *str1 != '\0' && *str2 != '\0' ) && *str1 == *str2 )
-    {
-        str1++;
-        str2++;
-    }
-
-    if(*str1 == *str2)
-    {
-        return 0; // strings are identical
-    }
-
-    else
-    {
-        return *str1 - *str2;
-    }
-}
 static const char* commands[] = {"help", "screenshot", "invalidopcode", "dividebyzero", "inforeg", "printmem", "time", "changefontsize", "tron"};
 static void (*commands_functions[])(char parameters[COMMANDS_LENGTH][LENGTH_PARAMETERS]) = {help, screenshot, invalidOPCode, divideByZero, inforeg, printMem, time, changeFontSize, tron};
 
@@ -71,10 +70,11 @@ static void (*commands_functions[])(char parameters[COMMANDS_LENGTH][LENGTH_PARA
 
 static int findIdxCommand(char *buff){
 	for (int i = 0; i < COMMANDS_LENGTH ; i++){
-		if ( strcmp(buff,commands[i]) == 0 ){
+		if (_strcmp(buff, commands[i]) == 0){
 			return i;
 		}
 	}
+	print("Command not found: try again\n");
 	return -1; //command not found
 }
 
@@ -83,30 +83,22 @@ int main() {
 	//Por ahora nos manejamos con syscalls pero habria que implementar la lib de C
 	//Es decir printf, scanf, etc
 
-	print("Welcome: type 'help' for options");
-	print("\n");
-	print("$>");
+	print("WELCOME! Type \"HELP\" for list\n");
 	while(1){
-		// usuario escribe comando -> paso a idx: lo busco en mi arrays cual es y ejecuto
-		
+		print("$>");
+		//recepcion de parametros ???????
 		//buffer para ver q comando me manda
 		char buff_command[BUFFER_LENGTH] = {0};
+		
 		scan(buff_command, BUFFER_LENGTH);  
-
+	
 		// paso a idx (fun aux)
 		int idx = findIdxCommand(buff_command);
-		if ( idx != -1){
-			//busco en mi array
-			// LE PASO PARAMETROS: VER COMO RECIBO
-			print("Lo encontro!");
-			//char parameters[COMMANDS_LENGTH][LENGTH_PARAMETERS];
-			//commands_functions[idx](parameters);
+		if (idx==0){
+			help();
 		}
-		else{
-			print("Command not found: try again");
-		}
+		
 		  
 	}
 	return 0;
-	
 }
