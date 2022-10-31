9b6f8ec62551ce9346ad42f9f5a74fe69319ecb9
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-31 18:35:02 -0300

    tron in process

--- Userland/SampleCodeModule/tron.c
+++ Userland/SampleCodeModule/tron.c
@@ -9,6 +9,7 @@ typedef struct {
     uint64_t posX;
     uint64_t posY;
     uint8_t direction;
+    uint8_t state;
     Color color;    
 } Player;
 
@@ -49,9 +50,69 @@ void do_paintRect(uint64_t fromX, uint64_t fromY, uint64_t width, uint64_t heigh
     return sys_paint_rect(fromX, fromY, height, width, color);
 }
 
-static int isAlive(Player * p, uint16_t width, uint16_t height, uint8_t **board) {
-    
+//ESTAMOS PONIENDO BOARD COMO B[Y][X] 
+//BOARD NO TIENE LOS BORDES EN CUENTA 
+static int isAlive(Player  p, uint16_t width, uint16_t height, uint8_t ** board) {
+    return p.posX < height && p.posX > 0 && p.posY < width && p.posY > 0 && !board[p.posY][p.posX]; //chequeo rango
 }
+
+static void updatePosition(uint64_t keypressed){
+    switch (keypressed)
+            {
+            // caso default no existe ya q tiene q hacer algo siempre
+            case 'w':
+                if (player1.direction != 's'){ //opposite 
+                    player1.posY--;
+                    player1.direction = 'w';
+                }
+                break;
+            case 'a':
+                if(player1.direction != 'd'){
+                    player1.posX++;
+                    player1.direction = 'a';
+                }
+                break;
+            case 's':
+                if(player1.direction != 'w'){
+                    player1.posY++;   
+                    player1.direction = 's';
+                }
+                break;
+            case 'd':
+                if (player1.direction != 'a'){
+                    player1.posX++;
+                    player1.direction = 'd';
+                }
+                break;
+            //Player 2
+            case 'U': //up
+                if (player2.direction != 'D'){ //opp opp?
+                    player2.posY--;
+                    player2.direction = 'U';
+                }
+                break;
+            case 'L': //left
+                if(player2.direction != 'R'){
+                    player2.posX--;
+                    player2.direction = 'L';
+                }
+                break;
+            case 'D': //down
+                if(player2.direction != 'U'){
+                    player2.posY++;   
+                    player2.direction = 'D';
+                }
+                break;
+            case 'R': //right
+                if (player2.direction != 'L'){
+                    player2.posX++;
+                    player2.direction = 'R';
+                }
+                break;
+            }
+}
+
+
 
 //ejecuta el tron
 void play_tron(){
@@ -61,16 +122,30 @@ void play_tron(){
     getScreenData(screen);
     setScreen(screen[0], screen[1]);
     setPlayers(screen[0],screen[1]);
-    uint8_t alive1 = 1, alive2 = 1;
+    
+    const int8_t width = (screen[0]-16)/8;
+    const uint8_t height = (screen[1]-16)/8;
+    uint8_t board[width][height]; //me pa q 
+    for(int i = 0; i < width;i++) for(int j = 0; j< height; j++)  board[i][j]=0; 
+    //uint8_t alive1 = 1, alive2 = 1;
     uint64_t speed = 3;
     uint64_t ticks = sys_getTicks();
     uint64_t lastTicks = 0;
-    while(alive1 && alive2){
-        if (lastTicks != ticks && ticks % speed == 0){
-            do_paintRect(player1.posX, player1.posY, SIZE, SIZE, player1.color);
-            do_paintRect(player2.posX, player2.posY, SIZE, SIZE, player2.color);
-            player1.posX+=SIZE;
-            player2.posX-=SIZE;
+    while(player1.state /*= isAlive(player1, screen[0], screen[1], board)*/ && player2.state){
+        char c = getChar();
+        updatePosition(c);
+        if (lastTicks != ticks && ticks % speed == 0){ 
+             //este getchar espera enter 
+            //desps lo vemos pq usa el sys_read y eso tiene para esperar enter
+            //haces el switch y player1.posY += SIZE ponele
+            //board[player1.posY / SIZE][player1.posX / SIZE] = 1;
+            //board[player2.posY / SIZE][player2.posX / SIZE] = 1; check q /size da bien!
+            updatePosition(player1.direction);
+            updatePosition(player2.direction);
+            do_paintRect((player1.posX)*SIZE, (player1.posY) *SIZE, SIZE, SIZE, player1.color);
+            do_paintRect((player2.posX)*SIZE, (player2.posY)*SIZE, SIZE, SIZE, player2.color);
+            board[player1.posY][player1.posX]=1;
+            board[player2.posY][player2.posX]=1;
             lastTicks = ticks;
         }
         ticks = sys_getTicks();
@@ -112,10 +187,10 @@ void play_tron(){
 
 
 static void paintBorders(uint16_t width, uint16_t height, Color color) {
-    do_paintRect(0, 0, width, SIZE, white);             // arr 
-    do_paintRect(0, 0, SIZE, height, white);            // izq
-    do_paintRect(width-SIZE, 0, SIZE, height, white);   // der
-    do_paintRect(0, height-SIZE, width, SIZE, white);   // abajo
+    do_paintRect(0, 0, width, SIZE, white);             // top border 
+    do_paintRect(0, 0, SIZE, height, white);            // left border
+    do_paintRect(width-SIZE, 0, SIZE, height, white);   // right bor
+    do_paintRect(0, height-SIZE, width, SIZE, white);   // bottom border
 }
 
 //hace la ventana en donde se juega al tron
@@ -123,25 +198,24 @@ void setScreen(uint64_t width, uint64_t height){
     Color bg_color = {0x3B, 0x0A, 0x00}; //azul
     do_clearScreen(bg_color); //pintar pantalla d azul
     paintBorders(width, height, tronColor3);
-
-    //board[0][0] = 1 . supizq: (8,8) infder: (16,16)
-    //chequear cto sacamos (ESTA EN TAM PIXEL)
 }
 
 
 void setPlayers(uint16_t width, uint16_t height){
 
-    player1.posX = width/4;
-    player1.posY = height/2;
-    player1.direction = right; //hacemos un enum con UP, DOWN, LEFT, RIGHT
+    player1.posX = (width/4)/8;
+    player1.posY = (height/2)/8;
+    player1.direction = 'd'; //hacemos un enum con UP, DOWN, LEFT, RIGHT
     player1.color = red;
+    player1.state = 1;
     //do_paintRect(player1.posX, player1.posY, SIZE, SIZE, green);             
     
 
-    player2.posX = width - width/4;
-    player2.posY = height/2;
-    player2.direction = left;
+    player2.posX = (width - width/4)/8;
+    player2.posY = (height/2)/8;
+    player2.direction = 'L';
     player2.color = green;
+    player2.state = 1;
     //do_paintRect(player2.posX, player2.posY, SIZE, SIZE, orange);            
 }
 
