88c5b4411ccff81fab49c430bf0212e59086d09c
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-31 18:35:40 -0300

    stringUtil.h

--- Userland/SampleCodeModule/include/stringUtil.h
+++ Userland/SampleCodeModule/include/stringUtil.h
@@ -5,7 +5,7 @@
 
 void _strcpy(char * dest, char * src, int n);
 
-uint64_t _strlen(char  str[]);
+uint64_t _strlen( char  str[]);
 
 int _strcmp(char *str1, char *str2);
 
@@ -14,5 +14,9 @@ uint64_t getFormat(uint64_t n);
 uint64_t itoa(uint64_t number, char* s);
 
 uint64_t hexStrToInt(char* s);
+
+int checkMem(char mem[]);
+
+uint64_t atoi(char S[]);
 
 #endif
