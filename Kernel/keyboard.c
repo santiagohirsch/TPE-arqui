f56af5f46792fc7b5a0c83ab764f461d2a6054c0
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-31 18:33:46 -0300

    keyboard

--- Kernel/keyboard.c
+++ Kernel/keyboard.c
@@ -3,12 +3,12 @@
 #include <stdint.h>
 #include <string.h>
 
-#define MAX_LENGTH 256
+#define BUFFER_SIZE 256
 
 
 static const uint8_t charHexMap[256] = {       
         0,    0,  '1',  '2',  '3',  '4',  '5',  '6',   '7',  '8',  '9',   '0',   '-',  '=',    0x7F,
-    '    ', 'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',   'o',  'p',  '[',   ']',  '\n',   
+    '    ', 'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',   'o',  'p',  '[',   ']', '\n',   
         0,     'a',  's', 'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  '\"',
     0,    0,  '\\',   'z',  'x',     'c', 'v', 'b',  'n',  'm',  ',',  '.',  '/',    0,  
     '*',     0,  ' ',    0,     0,     0/*60*/,    0,       0,         0, //yo creo q deberia ir ahi pero a //viendo el teclado... creo asjdjaj
@@ -17,56 +17,40 @@ static const uint8_t charHexMap[256] = {
 };
 //flechas: arriba abajo der izq para probar
 
-static uint8_t buffer[MAX_LENGTH] = {0};
-static uint64_t bufferLength = 0;
-static uint8_t lastChar;
+static uint8_t buffer[BUFFER_SIZE] = {0};
+static uint64_t qElements = 0;
+static uint64_t readIndex = 0;          // iter first element
+static uint64_t writeIndex = 0;         // iter last element
+
 
 void keyboard_handler(uint64_t teclahex){
-    if (teclahex < 0x53 ){
-        //aca en vez de imprimir charHexMap[] imprimi teclahex para ver el codigo q nos tira
-        ngc_printChar(charHexMap[teclahex]);  //
-        lastChar = charHexMap[teclahex];
-        if (lastChar == '\n'){
-            buffer[bufferLength] = '\0';
-        } else if (lastChar == 0x7F && bufferLength > 0){
-            bufferLength--;
-        } else if (lastChar != '\t'){
-            buffer[bufferLength++] = lastChar;
-        }   
-    }
-}
-
-char getLastChar(){
-    return lastChar;
-}
-
-
-static void clearBuffer(){
-    bufferLength = 0;
-    lastChar = '\0';
-}
-
-
-char * getBuffer(){ // aca adentro todo ok
-    char *out;
-    memcpy(out,buffer, bufferLength);
-    if (getLastChar() == '\n'){
-        clearBuffer();
-    }
-    return out;
-}
-
-
-char read_keyboard(){
-    uint64_t i=tKey();
-	uint64_t teclahex=i;
     if (teclahex < 0x53){
-        return charHexMap[teclahex];
-    } else {
-        return 'a';
+        if(qElements >= BUFFER_SIZE) return;  // buffer is full
+        
+        // MAKE IT ROUNDY
+        if (writeIndex >= BUFFER_SIZE)     writeIndex = 0;
+
+        buffer[writeIndex] = charHexMap[teclahex];
+
+        // update iterators
+        qElements++;
+        writeIndex++;
     }
 }
 
-uint64_t getBufferLength(){
-    return bufferLength;
+char getFirstChar(){
+    if (qElements == 0) { 
+        return -1;
+    }
+
+    char toReturn = buffer[readIndex];
+
+    // update iterators
+    qElements--;
+    readIndex++;
+
+    // MAKE IT ROUNDY
+    if (readIndex == BUFFER_SIZE) readIndex = 0;
+    
+    return toReturn;
 }
