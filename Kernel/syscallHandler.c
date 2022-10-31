f5518750cd33bc775e79e7de5d8d70df653999ae
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-31 18:34:14 -0300

    fix

--- Kernel/syscallHandler.c
+++ Kernel/syscallHandler.c
@@ -10,33 +10,23 @@ extern uint64_t info[17];
 extern uint8_t screenshot;
 
 
-static void sys_write_handler(unsigned int fd, const char * buffer, int bytes){
-    if (fd == STDOUT){
-        ngc_print(buffer); //deberia recibir los bytes
+static void sys_write_handler(uint64_t fd, const char * buffer, uint64_t bytes){
+    if (fd == STDOUT) {
+        for (uint64_t i = 0; i < bytes; i++){
+            ngc_printChar(buffer[i]);
+        }
     }
 }
 
-
-static void sys_read_handler(unsigned int fd,  char * buffer, int bytes){
-    if (fd == STDIN || fd == KBDIN){
-        uint64_t aux = 0;
-        int flag = 0;
-		while(!flag && *buffer != '\t' ){
-            _hlt();
-            if (getLastChar() == '\n'){
-                flag = 1;
-            }
-        }
-        uint64_t n = getBufferLength();
-        memcpy(buffer,getBuffer(),n);
-    }
+static void sys_read_handler(unsigned int fd, uint64_t buffer, int bytes){
+    if (fd != STDIN && fd != KBDIN) return;
+    _hlt();
+    ((char*)buffer)[0] = getFirstChar();
 }
 
 static uint64_t sys_time_handler(){
     return (_NRTCGetHours()) | ((uint64_t)_NRTCGetMins() << 8) | ((uint64_t)_NRTCGetSeconds() << 16);
 }
-
-
 
 static uint8_t sys_inforeg_handler(uint64_t regVec[17]){
     if (screenshot){
@@ -85,8 +75,17 @@ static void sys_screenData_handler(uint64_t * data){
 static uint64_t sys_ticks_handler(){
     return ticks_elapsed();
 }
-
-static void (* syscalls[30]) (uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t rax) = {sys_read_handler, sys_write_handler, sys_time_handler, sys_inforeg_handler, sys_font_handler, sys_printColor_handler, sys_clear_screen_handler, sys_screenData_handler, sys_paint_rect_handler, sys_ticks_handler};
+/*
+static void sys_timed_read_handler(char * buffer, uint64_t ticks){
+    uint8_t i = 0, limit = 256;
+    while(ticks_elapsed() < ticks && i < limit){
+        read_keyboard(buffer + i); //nos guarda en el buffer
+        i++;
+    }
+    buffer[i] = '\0';
+}
+*/
+static void (* syscalls[30]) (uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t rax) = {sys_read_handler, sys_write_handler, sys_time_handler, sys_inforeg_handler, sys_font_handler, sys_printColor_handler, sys_clear_screen_handler, sys_screenData_handler, sys_paint_rect_handler, sys_ticks_handler, /*sys_timed_read_handler*/};
 
 //  paso syscall_id por rax, se come r10 por rcx, y r9 por rax
 void syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t rax){
