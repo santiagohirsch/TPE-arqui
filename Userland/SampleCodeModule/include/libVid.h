9b0a457d978eab2ebd78bc1bad5adc59e2bac418
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-10-31 18:35:29 -0300

    redefinitions

--- Userland/SampleCodeModule/include/libVid.h
+++ Userland/SampleCodeModule/include/libVid.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <colors.h>
 
+void scan(char * buffer, uint64_t length);
+
+void printColor(const char * buffer, Color color);
+
 void printf(const char *format, ...);
 
 void scanf(char* format, ...);
@@ -19,5 +23,9 @@ void do_invalidOpCode();
 void printInfoReg();
 
 void do_changeFontSize(uint64_t level);
+
+char getChar();
+
+void putChar(char c);
 
 #endif
