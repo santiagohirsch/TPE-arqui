c2ca53e5fa51175a9204c72e88845fab689156bb
sofialtman <70671805+sofialtman@users.noreply.github.com>
2022-11-01 00:21:43 -0300

    working on tron's beep

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
