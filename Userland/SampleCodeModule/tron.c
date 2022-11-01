9f3a7e744cc60f72b2ac82f392475aa4216cd522
shirsch <shirsch@itba.edu.ar>
2022-11-01 15:29:08 -0300

    fixing bugs in tron

--- Userland/SampleCodeModule/tron.c
+++ Userland/SampleCodeModule/tron.c
@@ -61,7 +61,7 @@ static void updatePosition(uint64_t keypressed){
             {
             // caso default no existe ya q tiene q hacer algo siempre
             case 'w':
-                if (player1.direction != 's' /*&& player1.direction != 'w'*/){ //opposite 
+                if (player1.direction != 's' /* && player1.direction != 'w'*/){ //opposite 
                     player1.posY--;
                     player1.direction = 'w';
                 }
@@ -123,7 +123,7 @@ void play_tron(){
     setScreen(screen[0], screen[1]);
     setPlayers(screen[0],screen[1]);
     
-    const uint16_t width = (screen[0]-16)/8;
+    const uint16_t width = (screen[0]-16)/8 ;
     const uint16_t height = (screen[1]-16)/8;
     uint16_t board[width][height]; //me pa q 
     for(int i = 0; i < width;i++) for(int j = 0; j< height; j++)  board[i][j]=0; 
@@ -134,24 +134,29 @@ void play_tron(){
     while(player1.state && player2.state){
         char c = getChar();
         updatePosition(c);
+
+        board[player1.posX][player1.posY]=1;
+        board[player2.posX][player2.posY]=1;
+        do_paintRect((player1.posX)*SIZE, (player1.posY)*SIZE, SIZE, SIZE, player1.color);
+        do_paintRect((player2.posX)*SIZE, (player2.posY)*SIZE, SIZE, SIZE, player2.color);
         if (lastTicks != ticks && ticks % speed == 0){ 
              //este getchar espera enter 
             //desps lo vemos pq usa el sys_read y eso tiene para esperar enter
             //haces el switch y player1.posY += SIZE ponele
             //board[player1.posY / SIZE][player1.posX / SIZE] = 1;
             //board[player2.posY / SIZE][player2.posX / SIZE] = 1; check q /size da bien!
-            do_paintRect((player1.posX)*SIZE, (player1.posY)*SIZE, SIZE, SIZE, player1.color);
-            do_paintRect((player2.posX)*SIZE, (player2.posY)*SIZE, SIZE, SIZE, player2.color);
             updatePosition(player1.direction);
             updatePosition(player2.direction);
-            player1.state = (isAlive(player1, width, height) && !board[player1.posX][player1.posY]);
-            player2.state = (isAlive(player2, width, height)&& !board[player2.posX][player2.posY]);
+            player1.state = (isAlive(player1, width+1, height+1) && !board[player1.posX][player1.posY]);
+            player2.state = (isAlive(player2, width+1, height+1)&& !board[player2.posX][player2.posY]);
+            board[player1.posX][player1.posY]=1;
+            board[player2.posX][player2.posY]=1;
             if(player1.state && player2.state){
                 do_paintRect((player1.posX)*SIZE, (player1.posY)*SIZE, SIZE, SIZE, player1.color);
                 do_paintRect((player2.posX)*SIZE, (player2.posY)*SIZE, SIZE, SIZE, player2.color);
             }
-            board[player1.posX][player1.posY]=1;
-            board[player2.posX][player2.posY]=1;
+            
+            
             lastTicks = ticks;
         }
         ticks = sys_getTicks();
@@ -163,7 +168,7 @@ void play_tron(){
     do_paintRect(0,0,screen[0],screen[1]/SIZE*8,black);
     do_changeFontSize(2);
     
-    char c;
+    char d;
     if(player1.state==1){
         printf("\n\n\t\t\t\t\t\t\t\t   THE WINNER IS RED\n");
     }
@@ -175,13 +180,16 @@ void play_tron(){
     }
     printf("\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t   Press 'e' to go back to the terminal \n\t\t\t\t\t\t\t\t\t\tor 'p' to play again\n");
     do_changeFontSize(1);
-    while(c != 'e' && c != 'p'){
-        c = getChar();
+    while(d != 'e' && d != 'p'){
+        d = getChar();
+    }
+    if(d == 'p'){
+        play_tron();
+        return;
+    } else {
+        do_clearScreen(black);
     }
     
-    if(c == 'p')
-        play_tron();
-    do_clearScreen(black);
     return;
 }
 
